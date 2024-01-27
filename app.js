60c965066f56b3dbea3c6622526ba8328fc21f48
Saidesai099 <149484094+Saidesai099@users.noreply.github.com>
2024-01-28 00:14:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+/* 
+
+a = 5 
+b = 10
+
+a = 10
+b = 5
+
+*/
+
+// Swap variable
+
+var a = 69 ;
+var b = 96 ;
+
+
+let abhi;
+
+abhi = a;
+
+a = b;
+
+b = abhi;
+
+console.log(`This is a`, a)
+console.log(`This is b`, b)
+
+
+
+// 
+
